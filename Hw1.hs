f1d7803ec32c991a3c40fac17a2638288414501b
harunarmut <34441987+harunarmut@users.noreply.github.com>
2019-04-03 02:22:50 +0300

    Add files via upload

---
+++ Hw1.hs
@@ -0,0 +1,9 @@
+module Hw1 where
+
+type Mapping = [(String, String, String)]
+data AST = EmptyAST | ASTNode String AST AST deriving (Show, Read)
+
+writeExpression :: (AST, Mapping) -> String
+evaluateAST :: (AST, Mapping) -> (AST, String)
+-- DO NOT MODIFY OR DELETE THE LINES ABOVE -- 
+-- IMPLEMENT writeExpression and evaluateAST FUNCTION ACCORDING TO GIVEN SIGNATURES -- 
